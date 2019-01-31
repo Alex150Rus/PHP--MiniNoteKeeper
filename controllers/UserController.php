5bbdbfcfca1fd7c543f86189518e157e3a20ada3
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-01-31 20:49:10 +0300

    small-changes

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -9,10 +9,20 @@
 namespace app\controllers;
 
 
+use app\models\repositories\UserRepository;
+use app\models\User;
+
 class UserController extends Controller
 {
   public function actionIndex() {
     echo "здесь может отрисовываться форма, путь - домен/user";
+    // создаю нового пользователя с произвольными (сам придумал) данными
+    (new UserRepository)->save(new User(null, 'Vasya', '22@ru.ru'));
+  }
+
+  public function actionNote() {
+    //вот здесь буду ловить id пользователя uri будет /user/note/id=1;
+
   }
 
 }
