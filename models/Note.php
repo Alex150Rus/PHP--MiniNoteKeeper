492676d4d8b54583fc810addf78bb06b00455f37
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-01-31 14:58:34 +0300

    models created

---
+++ models/Note.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex1
+ * Date: 31.01.2019
+ * Time: 14:53
+ */
+
+namespace app\models;
+
+
+class Note
+{
+
+}
---
+++ models/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex1
+ * Date: 31.01.2019
+ * Time: 14:49
+ */
+
+class User
+{
+
+}
